23535f7a95c60184bcb48d946df707ca6f5f2278
Burakov Ivan <burakovivan@gmail.com>
2018-04-06 14:31:12 +0300

    Great changes coming soon.. (project not able to build)

---
+++ PubLibIS.ViewModels/BackupFile/BackupFileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubLibIS.ViewModels
+{
+    public class BackupFileViewModel
+    {
+        public class BackupFile
+        {
+            public int Id { get; set; }
+            public string FileName { get; set; }
+            public string User_Id { get; set; }
+        }
+    }
+}
